0667e480f521e370f1d4daf1b6c0f2a66dd0eee5
nethmidinanjana <nethmidinanjana02@gmail.com>
2025-04-01 12:26:21 +0530

    Remove Constants.java from Git tracking

--- app/src/main/java/lk/nd/cycler/Constants.java
+++
@@ -1,6 +0,0 @@
-package lk.nd.cycler;
-
-public class Constants {
-    public static final String MERCHANT_ID = "1221412";
-    public static final String MERCHANT_SECRET = "OTYzMjgwMjA5MzAzNTMwODQ0OTI2MTg5NTkyMTUzODkzODY5ODc=";
-}
